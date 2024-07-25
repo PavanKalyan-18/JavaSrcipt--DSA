e0fd3e7e111a5802c20ca847fe4c7cd3c0c301fb
PavanKalyan-18 <pavanklyn18@gmail.com>
2024-07-25 10:03:43 +0530

    Space added

--- Basics.js/addition.js
+++ Basics.js/addition.js
@@ -1,6 +1,7 @@
 //* Write a program to Add two Variables a and b (a and b is being provided by the user your task is to display the sum)
 
 // Solution
+
 function add(a,b){
     const sum = a + b;
     console.log(sum);
